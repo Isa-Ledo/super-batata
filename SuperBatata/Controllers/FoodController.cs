150d5cf4ec1a6c838dd4d51bd066eb040a304b58
isa-ledo <bella-toledo@hotmail.com>
2022-10-13 19:41:39 -0300

    projeto inicial

---
+++ SuperBatata/Controllers/FoodController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SuperBatata.Api.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SuperBatata.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoodController : ControllerBase
+    {
+        private List<Batata> Batatas = new List<Batata>()
+        {
+                new Batata
+                {
+                    Id = 1,
+                    Name = "tradicional",
+                    Price = 10.00M
+                },
+                new Batata
+                {
+                    Id = 2,
+                    Name = "Crinkle",
+                    Price = 10.00M
+                },
+                new Batata
+                {
+                    Id = 3,
+                    Name = "Smile",
+                    Price = 10.00M
+                }
+        };
+
+        [HttpGet]
+        public List<Batata> Get()
+        {
+            return Batatas;
+        }
+
+        [HttpGet("{id:int}")]
+        public Batata GetById(int id)
+        {
+            return Batatas.FirstOrDefault(batata => batata.Id == id);
+        }
+
+        [HttpPost()]
+        public void Post(Batata novaBatata)
+        {
+            Batatas.Add(novaBatata);
+        }
+    }
+}
---
+++ SuperBatata/Repositories/Configuration/SuperBatataDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperBatata.Api.Entities;
+
+namespace SuperBatata.Api.Repositories.Configuration
+{
+    public class SuperBatataDbContext : DbContext
+    {
+        public DbSet<Batata> Batata { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=SuperBatata;Trusted_Connection=True;");
+        }
+    }
+}
